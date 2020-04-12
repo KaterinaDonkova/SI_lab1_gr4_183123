09963955f4da7a5443f15a1e90b62ff6945d5ad0
KaterinaDonkova <donkovakaterina55@gmail>
2020-04-12 12:43:25 +0200

    color variable added

--- Point.java
+++ Point.java
@@ -1,6 +1,15 @@
 class Point {
 	String id
 	double x,y;
+    String color;
+
+
+    public Point(String id, String color, double x, double y){
+        this.id = id;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
 
 	//TODO add new variable
 
